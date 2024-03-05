1ddb8f98e7367c9be769519d4ec67108c65ce292
Avijit Bhattacharjee <avijit@avijit.myguest.virtualbox.org>
2024-03-05 20:15:13 +0530

    adding malware, secret, sonar, copyright check and main workflows

---
+++ build-scripts/copyright-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+current_year=$(date +"%Y")
+missing_copyright=false
+
+for file in $(find . -name "*.java"); do
+    first_line=$(head -n 1 "$file")
+    if [[ ! "$first_line" =~ ^\/\/.*avijit\ bhattacharjee.*$current_year ]]; then
+        echo "File $file is missing copyright comment"
+        missing_copyright=true
+    fi
+done
+
+if [ "$missing_copyright" = true ]; then
+    exit 1
+fi
