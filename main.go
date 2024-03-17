e678793fcdec83753717c9307e0b7cfc62ace260
Avijit Bhattacharjee <avijit@avijit.myguest.virtualbox.org>
2024-03-17 20:11:51 +0530

    creating folder strcuture and code for practice

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/avijit/pkg/leetcode"
+)
+
+func main() {
+	leetcode.Start()
+}
---
+++ pkg/leetcode/start.go
@@ -0,0 +1,11 @@
+package leetcode
+
+import (
+	"fmt"
+	"github.com/avijit/pkg/leetcode/problems"
+)
+
+func Start() {
+	fmt.Println("Solving LeetCode problems...")
+	problems.Twosum_solution()
+}
