f8e4ab455a42e2451b14a9fd7768ec21be8a1525
Avijit Bhattacharjee <avijit@avijit.myguest.virtualbox.org>
2024-04-06 19:04:36 +0530

    leetcode 139, 1366

--- pkg/leetcode/problems/rankTeams.go
+++ pkg/leetcode/problems/rankTeams.go
@@ -1,8 +1,6 @@
 // Copyright (c) avijit bhattacharjee 2024
 
-
 // https://leetcode.com/problems/rank-teams-by-votes/
-
 
 package problems
 
@@ -11,19 +9,15 @@ import (
 	"sort"
 )
 
-
 func RankTeams() {
-	var votes = []string{"WXYZ","XYZW"}
+	var votes = []string{"WXYZ", "XYZW"}
 	fmt.Println(rankTeams(votes))
-	
+
 }
-
-
 
 func rankTeams(votes []string) string {
 
 	var result = make(map[rune]int, len(votes[0]))
-
 
 	for _, vote := range votes {
 
@@ -31,24 +25,30 @@ func rankTeams(votes []string) string {
 			result[value] = result[value] + index
 		}
 	}
+	fmt.Println(result)
 
-	var pairs []struct{ key, value int }
-	for k, v := range result {
-		pairs = append(pairs, struct{ key, value int }{k, v})
+	keys := make([]rune, 0, len(result))
+
+	for key := range result {
+		keys = append(keys, key)
 	}
 
-	// Sort the slice based on the values
-	sort.Slice(pairs, func(i, j int) bool {
-		return pairs[i].value < pairs[j].value
+	fmt.Println(result)
+	fmt.Println(keys)
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return result[keys[i]] < result[keys[j]]
 	})
 
-	// Create a new map from the sorted slice
-	sortedMap := make(map[int]int)
-	for _, p := range pairs {
-		sortedMap[p.key] = p.value
+	fmt.Println(keys)
+	var res string
+
+	for _, value := range keys {
+		res = res + string(value)
 	}
 
-	fmt.Println(sortedMap)
+	fmt.Println(res)
+
 	return "hahaha"
-    
+
 }
